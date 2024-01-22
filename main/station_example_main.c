e29a26087633fb287b60b5753854f2b72b6234a6
jing314 <1507098537@qq.com>
2024-01-22 22:19:47 +0800

    Mycommit_1.1,pwm驱动完成，gpio驱动完成，iic驱动未完成

--- main/station_example_main.c
+++ main/station_example_main.c
@@ -15,6 +15,8 @@
 #include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
+#include "driver/ledc.h"
+#include "driver/gpio.h"
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
@@ -26,6 +28,18 @@
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
+#define MYFLY_PWM1_IONUM 47
+#define MYFLY_PWM2_IONUM 33
+#define MYFLY_PWM3_IONUM 34
+#define MYFLY_PWM4_IONUM 48
+
+#define MYFLY_IICSCL_IONUM 1
+#define MYFLY_IICSDA_IONUM 2
+
+#define MYFLY_LED1_IONUM 6
+#define MYFLY_LED2_IONUM 5
+#define MYFLY_LED3_IONUM 7
+
 #define EXAMPLE_ESP_WIFI_SSID      "2128"
 #define EXAMPLE_ESP_WIFI_PASS      "147258369"
 #define EXAMPLE_ESP_MAXIMUM_RETRY  CONFIG_ESP_MAXIMUM_RETRY
@@ -67,6 +81,11 @@ static EventGroupHandle_t s_wifi_event_group;
 static const char *TAG = "wifi station";
 
 static int s_retry_num = 0;
+/*
+配置结构体初始化
+*/
+ledc_timer_config_t ledc_timer;//定时器
+ledc_channel_config_t ledc_channel;//通道
 
 
 static void event_handler(void* arg, esp_event_base_t event_base,
@@ -157,15 +176,51 @@ void wifi_init_sta(void)//wifi客户端模式初始化
     }
 }
 
-void pwm_init_time(void)//pwm初始化
+void pwm_init_time(void)//pwm初始化，
 {
+    /*
+    pwm配置
+    */
+    ledc_timer.freq_hz = 5000;//PWM信号频率
+    ledc_timer.duty_resolution = LEDC_TIMER_13_BIT;//占空比分辨率
+    ledc_timer.speed_mode = LEDC_LOW_SPEED_MODE;// 定时器模式
+    ledc_timer.timer_num = LEDC_TIMER_0;// 定时器序号;使用哪个定时器0-3;
+    ledc_timer_config(&ledc_timer);
+    /*
+    输出引脚配置
+    */
+    ledc_channel.channel = LEDC_CHANNEL_1;// LED控制器通道号,
+    ledc_channel.duty = 8191;               //占空比，最大值8192
+	ledc_channel.gpio_num = MYFLY_PWM1_IONUM;				// LED控制器通道对应GPIO, 
+	ledc_channel.speed_mode = LEDC_LOW_SPEED_MODE; // 模式, 
+	ledc_channel.timer_sel = LEDC_TIMER_0;   		// 使用哪个定时器0-3
+    ledc_channel_config(&ledc_channel);
+    ledc_channel.gpio_num = MYFLY_PWM2_IONUM;				// LED控制器通道对应GPIO, 
+    ledc_channel.channel = LEDC_CHANNEL_2;// LED控制器通道号,
+    ledc_channel_config(&ledc_channel);
+    ledc_channel.gpio_num = MYFLY_PWM3_IONUM;				// LED控制器通道对应GPIO, 
+    ledc_channel.channel = LEDC_CHANNEL_3;// LED控制器通道号,
+    ledc_channel_config(&ledc_channel);
+    ledc_channel.gpio_num = MYFLY_PWM4_IONUM;				// LED控制器通道对应GPIO, 
+    ledc_channel.channel = LEDC_CHANNEL_4;// LED控制器通道号,
+    ledc_channel_config(&ledc_channel);
+
+    //其他引脚的初始化只需要更改gpio_num与channel即可
 
 }
 
 void gpio_init_normal(void)//通用gpio初始化函数
 {
+    gpio_config_t gpio_init_struct;
+    gpio_init_struct.pin_bit_mask = (1ull << MYFLY_LED1_IONUM)|(1ull << MYFLY_LED2_IONUM)|(1ull << MYFLY_LED3_IONUM),
+    gpio_init_struct.mode = GPIO_MODE_OUTPUT;//IO模式
+    gpio_config(&gpio_init_struct);
+}
+
+void iic_init_mpu6050(void){
 
 }
+
 static void tcp_client_task(void *pvParameters)//tcp/ip协议任务
 {
     char rx_buffer[128];
@@ -181,7 +236,6 @@ static void tcp_client_task(void *pvParameters)//tcp/ip协议任务
         dest_addr.sin_port = htons(port);
         addr_family = AF_INET;
         ip_protocol = IPPROTO_IP;
- 
         int sock =  socket(addr_family, SOCK_STREAM, ip_protocol);
         if (sock < 0) {
             ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
@@ -196,13 +250,15 @@ static void tcp_client_task(void *pvParameters)//tcp/ip协议任务
         }
         ESP_LOGI(TAG, "Successfully connected");
  
-        err = send(sock, payload, strlen(payload), 0);
+        err = send(sock, payload, strlen(payload), 0);//信息发送，Hello, I am ESP32 Client，表示完成配置
 		if (err < 0) {
 			ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
 			break;
 		}
- 
-        while (1) {
+        /*
+        上为标准化配置
+        */
+        while (1) {//此处开始正式进行tcp任务
  
             int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
             // Error occurred during receiving
@@ -231,10 +287,15 @@ static void tcp_client_task(void *pvParameters)//tcp/ip协议任务
 
 static void pwm_ctrol_task(void *pvParameters)//pwm控制任务
 {
-
+//移植pid算法
 }
 
 static void gpio_ctrol_task(void *pvParameters)//gpio控制任务
+{
+//负责引脚控制部分，LED指示灯
+}
+
+static void mpu6050_read_task(void *pvParameters)//获取mpu6050传感器的数据
 {
 
 }
@@ -250,7 +311,12 @@ void app_main(void)
 
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
     wifi_init_sta();
+    pwm_init_time();
+    gpio_init_normal();
+    iic_init_mpu6050();
+
     xTaskCreate(tcp_client_task,"tcp_client",4096,NULL,5,NULL);
     xTaskCreate(pwm_ctrol_task,"pwm_ctrol",4096,NULL,5,NULL);
     xTaskCreate(gpio_ctrol_task,"gpio_ctrol",4096,NULL,5,NULL);
+    xTaskCreate(mpu6050_read_task,"mpu6050_read",4096,NULL,5,NULL);
 }
