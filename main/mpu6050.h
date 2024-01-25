02d9f856ae59344f003029a16fe989e32b5390c1
jing314 <1507098537@qq.com>
2024-01-25 15:00:33 +0800

    1.25日修改，i2c设备进行中，未验证

---
+++ main/mpu6050.h
@@ -0,0 +1,20 @@
+#ifndef __MPU6050_READ_H__
+#define __MPU6050_READ_H__
+#include "APP.h"
+/*
+    IIC相关函数
+*/
+#define I2C_Init()
+#define I2C_Start()
+#define I2C_Stop()
+
+#define I2C_Write_Byte(byte)
+#define I2C_Read_Byte()
+#define I2C_Read_ACK()
+#define I2C_Write_ACK()
+
+
+
+
+
+#endif
